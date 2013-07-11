b39e6b7b2fadaeacbb1eecd39485a9483a45272f
casasoft <casasoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-11 08:39:23 +0000

    increased version
    git-svn-id: http://plugins.svn.wordpress.org/casasync/trunk@739109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 Plugin Name: CasaSync
 Plugin URI: http://immobilien-plugin.ch
 Description: Das WP Immobilien-Plugin für Ihre Website importiert Immobilien aus Ihrer Makler-Software!
-Version: 2.0
+Version: 2.1
 Author: Casasoft AG
 Author URI: http://casasoft.ch
 License: GPL2
