e89fa4234fa1530bc7563b60886f89e30e20bf65
Jens Stalder <jensstalder@gmail.com>
2013-08-14 09:03:11 +0200

    2.1.3 changes

--- changelog.php
+++ changelog.php
@@ -20,3 +20,13 @@ Moved it to WordPress
 ===
 * Fixed tiny slash error
 * Re-Commited translation files
+
+
+2.1.3
+===
+* new: {availabilitylabel}
+* new: Availability import 'planned', 'reserved', 'reference', 'under-construction' etc
+* new: {if_planned}, {!if_planned}
+* new: {if_reference}, {!if_reference}
+* bug: False translations "Bahnhof -> Autobahn"
+* bug: Genereral stability improvements
