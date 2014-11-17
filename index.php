3e7e7fd2e2aa58e8cf8fa5901b45d4784af9bc19
Jens Stalder <jensstalder@gmail.com>
2014-11-17 08:52:08 +0100

    3.0.8 pre

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ namespace CasaSync;
  *	Description:    Das WP Immobilien-Plugin für Ihre Website importiert Immobilien aus Ihrer Makler-Software!
  *	Author:         Casasoft AG
  *	Author URI:     http://casasoft.ch
- *	Version: 		3.0.7
+ *	Version: 		3.0.8
  *	Text Domain: 	casasync
  *	Domain Path: 	languages/
  *	License: 		GPL2
