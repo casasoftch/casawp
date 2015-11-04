50475cd5aa9f635e89676213063fe16254553d80
Jens Stalder <jensstalder@gmail.com>
2015-11-04 10:02:30 +0100

    npm cleanup for global

--- plugin-assets/global/src/gulpfile.js
+++ plugin-assets/global/src/gulpfile.js
@@ -1,13 +1,4 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
-var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
-var autoprefixer = require('gulp-autoprefixer');
-var rename = require("gulp-rename");
 
 gulp.task('js_assets', function() {
   gulp.src('./bower_components/featherlight/release/featherlight.min.js').pipe(gulp.dest('./../js'));
