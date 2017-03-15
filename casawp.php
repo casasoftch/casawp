da1782b064e633c56612bf1de897226d7f78f9c4
Jens Stalder <jens.stalder@casasoft.ch>
2017-03-15 09:03:56 +0100

    readme and version bump

--- casawp.php
+++ casawp.php
@@ -5,7 +5,7 @@
  *	Description:    Import your properties directly from your real-estate managment software!
  *	Author:         Casasoft AG
  *	Author URI:     https://casasoft.ch
- *	Version: 	    2.2.3
+ *	Version: 	    2.2.4
  *	Text Domain: 	casawp
  *	Domain Path: 	languages/
  *	License: 		GPL2
