fc50b8cb7bed412646b31f1dcd28bd8da192a4b7
Jens Stalder <jens.stalder@casasoft.ch>
2018-03-02 10:56:22 +0100

    copy for urls with query params where broken

--- classes/Import.php
+++ classes/Import.php
@@ -223,7 +223,35 @@ class Import {
   }
 
   public function casawpUploadAttachmentFromGateway($property_id, $fileurl){
-    $filename = '/casawp/import/attachment/externalsync/' . $property_id . '/' . basename($fileurl);
+    if (strpos($fileurl, '://')) {
+      $parsed_url = parse_url(urldecode($fileurl));
+    } else {
+      $parsed_url = [];
+    }
+    if (isset($parsed_url['query']) && $parsed_url['query']) {
+      $file_parts = pathinfo($parsed_url['path']);
+
+      $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : ''; 
+      $host     = isset($parsed_url['host']) ? $parsed_url['host'] : ''; 
+      $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : ''; 
+      $user     = isset($parsed_url['user']) ? $parsed_url['user'] : ''; 
+      $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : ''; 
+      $pass     = ($user || $pass) ? "$pass@" : ''; 
+      $path     = isset($parsed_url['path']) ? $parsed_url['path'] : ''; 
+
+      $extension = $file_parts['extension'];
+      $pathWithoutExtension = str_replace('.'.$file_parts['extension'], '', $path);
+
+      $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : ''; 
+      $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : ''; 
+      
+      $converted = $scheme.$user.$pass.$host.$port.$pathWithoutExtension . str_replace(['?', '&', '#', '='], '-', $query.$fragment) . '.'.$extension;
+
+      $filename = '/casawp/import/attachment/externalsync/' . $property_id . '/' . basename($converted);
+
+    } else {
+      $filename = '/casawp/import/attachment/externalsync/' . $property_id . '/' . basename($fileurl);
+    }
 
     //extention is required
     $file_parts = pathinfo($filename);
@@ -242,10 +270,14 @@ class Import {
           $this->transcript['attachments'][$property_id]["uploaded_from_gateway"] = array();
         }
         $this->transcript['attachments'][$property_id]["uploaded_from_gateway"][] = $filename;
-        $could_copy = copy($fileurl, CASASYNC_CUR_UPLOAD_BASEDIR . $filename );
+        if (strpos($fileurl, '://')) {
+          $could_copy = copy(urldecode($fileurl), CASASYNC_CUR_UPLOAD_BASEDIR . $filename );
+        } else {
+          $could_copy = copy($fileurl, CASASYNC_CUR_UPLOAD_BASEDIR . $filename );
+        }
         if (!$could_copy) {
-          $filename = false;
           $this->transcript['attachments'][$property_id]["uploaded_from_gateway"][] = 'FAILED: ' .$filename;
+          $filename = false;
         }
       }
     }
